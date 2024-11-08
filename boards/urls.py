aaaa0608d9d8ca155005eab70bc598147b963185
bm4706 <bm4706@naver.com>
2024-11-08 15:37:03 +0900

    회원가입시 이미지삽입기능 및 에러메세지 기능 구현

--- boards/urls.py
+++ boards/urls.py
@@ -1,11 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from boards import views
+
+from .views import ArticleView, ArticleDetailView, PostView
 from django.urls import path, include
 
 urlpatterns = [
-    path('posts/', views.ArticleView.as_view(), name='post-list-create'),
-    path('posts/<int:pk>/', views.ArticleDetailView.as_view(), name='post-detail'),
+    # API 기반 뷰 URL 설정
+    path('api/posts/', ArticleView.as_view()),  # 게시글 목록 조회 및 생성 (API)
+    path('api/posts/<int:pk>/', ArticleDetailView.as_view()),  # 게시글 상세 조회, 수정, 삭제 (API)
+
+    # HTML 기반 뷰 URL 설정
+    path('posts/', PostView.as_view(), name='post-list-create'),  # 게시글 목록 조회 및 생성
+    path('posts/create/', PostView.as_view(), name='post-create'),  # 게시글 생성
+    path('posts/<int:pk>/', PostView.as_view(), name='post-detail'),  # 게시글 상세 조회
+    path('posts/<int:pk>/edit/', PostView.as_view(), name='post-edit'),  # 게시글 수정
+    path('posts/<int:pk>/delete/', PostView.as_view(), name='post-delete'),  # 게시글 삭제
 ]
 
 if settings.DEBUG:
--- boards/views.py
+++ boards/views.py
@@ -1,12 +1,23 @@
+from django.http import HttpResponseForbidden
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import status, permissions
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
 
 from rest_framework.exceptions import PermissionDenied, NotFound
-# Create your models here.
+
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
+
+### apiview 기반
 class ArticleView(APIView):
     
     # 권한 관리 (로그인시 읽기쓰기가능, 비로그인시 읽기만)
@@ -70,3 +81,61 @@ class ArticleDetailView(APIView):
         user.point -= 5
         user.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+### html 기반
+@method_decorator(login_required, name='dispatch')
+class PostView(View):
+    def get(self, request, pk=None):
+        if pk:
+            # 게시글 상세 조회
+            post = get_object_or_404(Post, pk=pk)
+            return render(request, 'boards/post_detail.html', {'post': post})
+        else:
+            # 게시글 목록 조회
+            posts = Post.objects.all().order_by('-created_at')
+            return render(request, 'boards/post_list.html', {'posts': posts})
+
+    def post(self, request, pk=None):
+        if pk:
+            # 게시글 수정
+            post = get_object_or_404(Post, pk=pk)
+            if post.author != request.user:
+                return HttpResponseForbidden("수정 권한이 없습니다.")
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('post-detail', pk=post.pk)
+        else:
+            # 게시글 생성
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                # 포인트 증가
+                request.user.point += 5
+                request.user.save()
+                return redirect('post-detail', pk=post.pk)
+        form = PostForm()
+        return render(request, 'boards/post_form.html', {'form': form})
+
+    def delete(self, request, pk):
+        # 게시글 삭제
+        post = get_object_or_404(Post, pk=pk)
+        if post.author != request.user:
+            return HttpResponseForbidden("삭제 권한이 없습니다.")
+        post.delete()
+        # 포인트 감소
+        request.user.point -= 5
+        request.user.save()
+        return redirect('post-list-create')
+
+
